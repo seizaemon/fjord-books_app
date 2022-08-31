16918469ab25d4baad8faac2c2f135e916a57978
seizaemon <oden@venus.dti.ne.jp>
2022-08-31 23:03:07 +0900

    fix: ロケール切替スイッチの翻訳を忘れていた

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :ja]
   end
 end
