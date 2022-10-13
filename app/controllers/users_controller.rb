05d56c545a34782a86ab85abd051fb69d59bb258
seizaemon <oden@venus.dti.ne.jp>
2022-10-14 01:10:47 +0900

    fix: ユーザー一覧画面は権限を気にしなくてよい

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show]
 
   # GET /users
   def index
@@ -11,13 +10,6 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    if current_user.id != params[:id].to_i
-      flash[:alert] = '閲覧権限がありません'
-      redirect_to users_path
-    end
-  end
-
-  def set_user
     @user = User.find(params[:id])
   end
 end
