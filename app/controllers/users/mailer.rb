8faba484dbb285e41014e67985f34ad5a5ef7395
seizaemon <oden@venus.dti.ne.jp>
2022-10-25 21:28:41 +0900

    fix: パスワードリセットのメールを日本語化

---
+++ app/controllers/users/mailer.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Users::Mailer < Devise::Mailer
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    passwords: 'users/passwords',
+    mailer: 'users/mailer'
   }
   resources :books, :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
