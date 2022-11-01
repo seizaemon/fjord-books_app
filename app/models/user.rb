d9c6adebffc1c13cd0ae4f2a87c0149c043ca183
seizaemon <oden@venus.dti.ne.jp>
2022-11-02 00:45:10 +0900

    feat: 画像フォーマットのフィルタを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :icon
+  validate :icon_image_type
+
+  private
+
+  def icon_image_type
+    format_allowed = %w[image/gif image/jpeg image/png]
+    errors.add :icon, :invalid_format, message: I18n.t('activerecord.models.errors.icon.invalid_format') unless format_allowed.include?(icon.blob.content_type)
+  end
 end
