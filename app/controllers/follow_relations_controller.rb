2c0aa0ad01fcc54dbea885437bcffd8de43e528d
seizaemon <oden@venus.dti.ne.jp>
2023-02-28 10:13:35 +0900

    fix: 指摘事項修正

--- app/controllers/follow_relations_controller.rb
+++ app/controllers/follow_relations_controller.rb
@@ -3,13 +3,13 @@
 class FollowRelationsController < ApplicationController
   def create
     user = User.find(params[:follower_id])
-    current_user.follow user
+    current_user.follow user unless current_user.following?(user)
     redirect_to followings_user_path(current_user)
   end
 
   def destroy
     user = User.find(params[:follower_id])
-    current_user.unfollow user
+    current_user.unfollow user if current_user.following?(user)
     redirect_to followings_user_path(current_user)
   end
 end
